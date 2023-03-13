28f0e871adf1a70f7f59e7aebc80448f2cbb4d9d
Dandddy <da.3.id7@gmail.com>
2023-03-13 16:22:49 +0200

    Изменил систему перехода между канвасами, рефакторинг кода

---
+++ Assets/Cars/UI/Scripts/Screnes/ScreenController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Cars.UI
+{
+    public class ScreenController : MonoBehaviour
+    {
+        [SerializeField] private string _tagScreen;
+
+        public void OpenScreen(GameObject openScreen)
+        {
+            CloseLastScreen();
+            openScreen.SetActive(true);
+        }
+
+        private void CloseLastScreen()
+        {
+            GameObject go = GameObject.FindWithTag(_tagScreen);
+            go.SetActive(false);
+        }
+    }
+}
