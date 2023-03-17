f5e694cf621942e1e6334ac16174889f80ccc922
Dandddy <da.3.id7@gmail.com>
2023-03-17 16:12:58 +0200

    Спавн машинки и управление

---
+++ Assets/Cars/Objects/Player/Scripts/CreatePlayer.cs
@@ -0,0 +1,30 @@
+﻿using IJunior.TypedScenes;
+using UnityEngine;
+
+public class CreatePlayer : MonoBehaviour, ISceneLoadHandler<int>
+{
+    [SerializeField] private GameObject _car;
+    [SerializeField] private Transform _spawn;
+    [SerializeField] private Sprite[] _sprites;
+
+    public void OnSceneLoaded(int index)
+    {
+        CreateCar(index);
+    }
+
+    private void CreateCar(int index)
+    {
+        if (_car != null)
+        {
+            GameObject car = Instantiate(_car, _spawn.transform.position, Quaternion.identity);
+
+            var sprite = car.GetComponent<SpriteRenderer>();
+            sprite.sprite = _sprites[index];
+
+            if (index == 3)
+            {
+                car.GetComponent<Transform>().localScale = new Vector2(1.2f, 1.2f);
+            }
+        }
+    }
+}
---
+++ Assets/Cars/Objects/Player/Scripts/PlayerMove.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Vector2 _touch;
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Application.isMobilePlatform)
+            MoveMobile();
+        else if (Application.isEditor)
+            MoveEditor();
+    }
+
+    private void MoveMobile()
+    {
+        if (Input.touchCount > 0)
+        {
+            _touch = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            transform.position = SetPosition(_touch);
+        }
+    }
+
+    private void MoveEditor()
+    {
+        var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (IsPlayerInBoundaries(position))
+            return;
+
+        transform.position = SetPosition(position);
+    }
+
+    private bool IsPlayerInBoundaries(Vector2 position)
+    {
+        if (position.x < -2 || position.x > 2)
+            return true;
+        else if (position.y < -4 || position.y > 0)
+            return true;
+        else
+            return false;
+    }
+
+    private Vector2 SetPosition(Vector2 vector)
+    {
+        return new Vector2(vector.x, vector.y);
+    }
+}
