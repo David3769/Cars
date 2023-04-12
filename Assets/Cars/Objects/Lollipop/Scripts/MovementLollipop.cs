cb401a5304d63d06352ae2f0dda57642061dee63
Dandddy <da.3.id7@gmail.com>
2023-04-12 16:08:29 +0300

    Фикс бага с отображением чупика на дороге Сделал отображение сколько чупиков будет собрано и рандом цвет его

--- Assets/Cars/Objects/Lollipop/Scripts/MovementLollipop.cs
+++ Assets/Cars/Objects/Lollipop/Scripts/MovementLollipop.cs
@@ -15,15 +15,28 @@ namespace Cars.Game
 
         [SerializeField] private Transform[] _respawnPoints;
         [SerializeField] private float _speed;
-        [SerializeField] private int[] _addLollipops;
         [SerializeField] private float _yPositionForDelete;
-        
+        [SerializeField] private Sprite[] _spriteLollipops;
+        [SerializeField] private Sprite[] _spriteSticks;
+        [SerializeField] private SpriteRenderer _chups;
+        [SerializeField] private SpriteRenderer _stick;
+
         private BoostEffect _effect;
+        private int _addLollipop = 1;
 
         private void Start()
         {
             if (_effect == null)
                 _effect = BoostEffect.Instance;
+            SetNewValues();
+        }
+
+        private void SetNewValues()
+        {
+            _addLollipop = Random.Range(1, 4);
+            _chups.sprite = _spriteLollipops[_addLollipop - 1];
+            _chups.color = new Color(Random.value, Random.value, Random.value, 1);
+            _stick.sprite = _spriteSticks[_addLollipop - 1];
         }
 
         private void Update()
@@ -41,6 +54,7 @@ namespace Cars.Game
         public void Respawn()
         {
             transform.position =_respawnPoints[Random.Range(0, _respawnPoints.Length)].position;
+            SetNewValues();
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
@@ -51,18 +65,21 @@ namespace Cars.Game
                 {
                     _effect.SubtractEffect();
                     AddLollipop((int)AddCountLollipop.X2);
+                    Respawn();
                 }
                 else
+                {
                     AddLollipop((int)AddCountLollipop.Default);
+                    Respawn();
+                }
             }
         }
 
         private void AddLollipop(int boost)
         {
-            var add = _addLollipops[Random.Range(0, _addLollipops.Length)] * boost;
+            var add = _addLollipop * boost;
             PlayerDataHandler.Instance.AddLollipop(add);
             LollipopUIDraw.Instance.UpdateLollipop();
-            Respawn();
         }
     }
 }
