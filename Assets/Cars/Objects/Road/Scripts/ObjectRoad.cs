fd443c9c5e4f395860188e3d83e262d71b7fe3bc
Dandddy <da.3.id7@gmail.com>
2023-04-12 18:03:29 +0300

    Сделал спавн обьектов трассы

---
+++ Assets/Cars/Objects/Road/Scripts/ObjectRoad.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Cars.Game
+{
+    public class ObjectRoad : MonoBehaviour
+    {
+        private float _speed = 1f;
+
+        private void Update()
+        {
+            _speed = RoadDrive.Instance.Speed;
+            if (transform.position.y < -7f)
+                Destroy(gameObject);
+            transform.Translate(0, -_speed * Time.deltaTime, 0);
+        }
+    }
+}
---
+++ Assets/Cars/Objects/Road/Scripts/SpawnObjectsRoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cars.Game
+{
+    public class SpawnObjectsRoad : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] _objects;
+        [SerializeField] private float _minTimeSpawn;
+        [SerializeField] private float _maxTimeSpawn;
+        [SerializeField] private Transform[] _spawnPoints;
+
+        private void Start()
+        {
+            StartCoroutine(SpawnObjects());
+        }
+
+        private IEnumerator SpawnObjects()
+        {
+            while (true)
+            {
+                Instantiate(_objects[Random.Range(0, _objects.Length)],
+                    _spawnPoints[Random.Range(0, _spawnPoints.Length)].position,
+                    Quaternion.identity);
+                yield return new WaitForSeconds(Random.Range(_minTimeSpawn, _maxTimeSpawn));
+            }
+        }
+    }
+}
